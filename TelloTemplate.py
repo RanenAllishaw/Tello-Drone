d9393716f432a7395374fe7338dbbb389f87850a
Ranen Allishaw <ranens.work@gmail.com>
2022-12-21 08:29:02 -0500

    Square still not working

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -4,14 +4,11 @@
 
 import threading, socket, sys, time, subprocess
 
-
 # GLOBAL VARIABLES DECLARED HERE....
 host = ''
 port = 9000
-locaddr = (host,port)
-tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
-
-
+locaddr = (host, port)
+tello_address = ('192.168.10.1', 8889)  # Get the Tello drone's address
 
 # Creates a UDP socketd
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,22 +23,34 @@ def recv():
             data, server = sock.recvfrom(1518)
             print(data.decode(encoding="utf-8"))
         except Exception:
-            print ('\n****Keep Eye on Drone****\n')
+            print('\n****Keep Eye on Drone****\n')
             break
 
 
-def sendmsg(msg, sleep = 6):
+def sendmsg(msg, sleep=8):
     print("Sending: " + msg)
     msg = msg.encode(encoding="utf-8")
     sock.sendto(msg, tello_address)
     time.sleep(sleep)
+
 
 # recvThread create
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
 
-# CREATE FUNCTIONS HERE....
+# Square Function
+def Square():
+    for i in range(4):
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+
+
+# Triangle Function
+def Triangle():
+    for i in range(3):
+        sendmsg('forward 100')
+        sendmsg('cw 60')
 
 
 print("\nRanen Allishaw")
@@ -51,9 +60,6 @@ print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
 
-if ready == "no":
-    print('\noh ok')
-
 try:
     if ready.lower() == 'yes':
         print("\nStarting Drone!\n")
@@ -61,8 +67,7 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
+        Square()
 
         sendmsg('land')
 
